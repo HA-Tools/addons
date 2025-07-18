7a0b510d6ad5285c54000d0864bb2934438b8219
Freddy Greve <freddy@fred-dienste.de>
2025-07-18 17:46:01 +0200

    Update run.sh

--- ha-tools-tunnel/run.sh
+++ ha-tools-tunnel/run.sh
@@ -2,9 +2,18 @@
 
 CONFIG_PATH=/data/frpc.ini
 
-export server_addr="${SERVER_ADDR}"
-export server_port="${SERVER_PORT}"
-export subdomain="${SUBDOMAIN}"
+# Hole Variablen von Add-on-Options (Home Assistant setzt diese als Umgebungsvariablen)
+export server_addr="$SERVER_ADDR"
+export server_port="$SERVER_PORT"
+export subdomain="$SUBDOMAIN"
 
+echo "Generiere frpc.ini mit:"
+echo "  server_addr: $server_addr"
+echo "  server_port: $server_port"
+echo "  subdomain:   $subdomain"
+
+# Erzeuge Konfiguration
 envsubst < /frpc.ini.j2 > $CONFIG_PATH
+
+# Starte frpc
 exec frpc -c $CONFIG_PATH
