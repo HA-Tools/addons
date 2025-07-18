a9a50d570e801fa6333f31c6ffa1c4a8f4767fa3
Freddy Greve <freddy@fred-dienste.de>
2025-07-18 18:02:37 +0200

    Update run.sh

--- ha-tools-tunnel/run.sh
+++ ha-tools-tunnel/run.sh
@@ -2,19 +2,15 @@
 
 CONFIG_PATH=/data/frpc.yaml
 
-export SERVER_ADDR="${SERVER_ADDR}"
-export SERVER_PORT="${SERVER_PORT}"
-export SUBDOMAIN="${SUBDOMAIN}"
-
-# Debug: Zeige Umgebungsvariablen
 echo "FRPC wird gestartet mit:"
 echo "  server_addr: $SERVER_ADDR"
 echo "  server_port: $SERVER_PORT"
 echo "  subdomain:   $SUBDOMAIN"
 
-# Template verarbeiten
-export SERVER_ADDR SERVER_PORT SUBDOMAIN
 envsubst < /frpc.yaml.j2 > $CONFIG_PATH
 
-# Start
+echo "-------- Generierte frpc.yaml --------"
+cat $CONFIG_PATH
+echo "--------------------------------------"
+
 exec frpc -c $CONFIG_PATH
