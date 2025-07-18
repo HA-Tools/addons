d05d9bd36bfe9c031a15787a7ba23c676e0975b5
Freddy Greve <freddy@fred-dienste.de>
2025-07-18 18:11:38 +0200

    Update run.sh

--- ha-tools-tunnel/run.sh
+++ ha-tools-tunnel/run.sh
@@ -1,13 +1,14 @@
 #!/bin/bash
+set -e
 
 CONFIG_PATH=/data/frpc.yaml
 
 echo "FRPC wird gestartet mit:"
-echo "  server_addr: $SERVER_ADDR"
-echo "  server_port: $SERVER_PORT"
-echo "  subdomain:   $SUBDOMAIN"
+echo "  SERVER_ADDR: $SERVER_ADDR"
+echo "  SERVER_PORT: $SERVER_PORT"
+echo "  SUBDOMAIN:   $SUBDOMAIN"
 
-# explizite Ersetzung nur dieser Variablen:
+# Ersetze nur diese drei Variablen – so bleibt nichts übrig
 envsubst '$SERVER_ADDR $SERVER_PORT $SUBDOMAIN' < /frpc.yaml.j2 > $CONFIG_PATH
 
 echo "-------- Generierte frpc.yaml --------"
